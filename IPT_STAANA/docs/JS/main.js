4bf103a631d8890a131b91f045f0fc869a9a7473
BSIT3E-STAANA <isaacstaana8@gmail.com>
2024-11-26 17:27:14 +0800

    Add files via upload
    PORTFOLIO

---
+++ IPT_STAANA/docs/JS/main.js
@@ -0,0 +1,15 @@
+const spans = document.querySelectorAll('.progress-bar span');
+
+spans.forEach((span) => {
+    span.style.width = span.dataset.width;
+    span.innerHTML = span.dataset.width;
+});
+function downloadCV() {
+    const cvUrl = 'C:\Users\ADMIN\OneDrive\Desktop\index\cv\cv.pdf'; 
+    const link = document.createElement('a');
+    link.href = cvUrl;
+    link.download = 'Your_CV.pdf'; 
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
